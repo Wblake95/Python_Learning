d03fb029d3980b7511e93f1ceccd1af9c68909e4
Wesley Blake <wesleyblake95@gmail.com>
2022-06-18 18:42:00 -0700

    Update teaching_binarysearch.py

--- teaching_binarysearch.py
+++ teaching_binarysearch.py
@@ -25,5 +25,18 @@ elif (x.strftime("%p")) == "AM":
     print("Good morning!")
 elif (x.strftime("%p")) == "PM":
     print("Good afternoon")
+print("What is Binary Search?")
+print("Binary Search is when you take a list, like a bookshelf, and search through it to find the thing you want!")
+print("All you have to do is find out how long the list is and pic the middle number.")
+print("After than compare your number to the one you picked.")
+print("Lets give it a try!")
 
-guessme(10)
+guessme(5)
+
+print("So, how did you do?")
+x = input("How many tries till you got it? ")
+if x > 2:
+    print("That was an unlucky guess!")
+if x < 2:
+    print("Good job!")
+print("This idea can help you alot in the world. It can also help when learning to code.")
