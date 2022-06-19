a58ec96ca5aafe1db08720e22805b81a919b321c
Wesley Blake <wesleyblake95@gmail.com>
2022-06-18 18:59:39 -0700

    Update teaching_binarysearch.py

--- teaching_binarysearch.py
+++ teaching_binarysearch.py
@@ -8,14 +8,16 @@ def guessme(x):
     rand = random.randint(1,x)
 
     while guess != rand:
-        print(f"Guess a number between 1 and {x}")
-        print("What is your first guess?")
+        print(f"\n\n\nGuess a number between 1 and {x}")
+        print("What is your guess?")
         guess = int(input())
         if guess < rand:
             print("Your guess was to low!")
+        elif guess < rand:
+            print("Your guess is really low!")
         elif guess > rand:
             print("Your guess was to high!")
-    print("You guessed correctly!")
+    print("\n\n\nYou guessed correctly!")
 
 
 x = dt.datetime.now()
@@ -25,16 +27,20 @@ elif (x.strftime("%p")) == "AM":
     print("Good morning!")
 elif (x.strftime("%p")) == "PM":
     print("Good afternoon")
-print("What is Binary Search?")
+
+print("\nWhat is Binary Search?")
 print("Binary Search is when you take a list, like a bookshelf, and search through it to find the thing you want!")
 print("All you have to do is find out how long the list is and pic the middle number.")
-print("After than compare your number to the one you picked.")
+print("After that compare your number to the one you picked.")
+print("Then repeat that until you get the number you were looking for.\n\n")
 print("Lets give it a try!")
+
 
 guessme(5)
 
+
 print("So, how did you do?")
-x = input("How many tries till you got it? ")
+x = int(input("How many tries till you got it? "))
 if x > 2:
     print("That was an unlucky guess!")
 if x < 2:
