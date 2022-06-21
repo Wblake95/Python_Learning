40716fe1726a74f8c733a6e01b6f9064b6eb62ee
Wesley Blake <wesleyblake95@gmail.com>
2022-06-21 15:16:42 -0700

    Update importer.py

--- python_troubleshooting/import_errors/importer.py
+++ python_troubleshooting/import_errors/importer.py
@@ -1,1 +1,16 @@
 #! /usr/bin/env python3
+
+try:
+#trying to find a module in the parent package
+    from home import words
+    print(words.debug)
+    del words
+except ImportError:
+print("Relative import failed")
+
+try:
+#Trying to find module on sys.path
+    import words
+    print(words.debug)
+except ModuleNotFoundError:
+print("Absolute import failed")
