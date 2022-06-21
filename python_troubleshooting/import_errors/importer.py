590849b3542d0d2d5f9f73f2b5a5d196f6447141
Wesley Blake <wesleyblake95@gmail.com>
2022-06-21 15:18:45 -0700

    Update importer.py

--- python_troubleshooting/import_errors/importer.py
+++ python_troubleshooting/import_errors/importer.py
@@ -2,6 +2,7 @@
 
 try:
 #trying to find a module in the parent package
+# This one didn't work. I am not sure when I would use this. But I have it now.
     from home import words
     print(words.debug)
     del words
@@ -9,7 +10,8 @@ except ImportError:
 print("Relative import failed")
 
 try:
-#Trying to find module on sys.path
+#Trying to find module on path
+# This one works for hangman. I think it is because it is on the same path.
     import words
     print(words.debug)
 except ModuleNotFoundError:
