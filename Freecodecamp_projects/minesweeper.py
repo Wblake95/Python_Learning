9e28e9fcd5fd772e8da0bc15b993239d6fe30900
Wesley Blake <wesleyblake95@gmail.com>
2022-07-05 10:44:55 -0700

    Add files via upload
    Failed project
    
    Didn't understand
    - Objects
    - How to print Objects

--- Freecodecamp_projects/minesweeper.py
+++ Freecodecamp_projects/minesweeper.py
@@ -6,21 +6,28 @@
 # https://www.youtube.com/watch?v=8ext9G7xspg&t=5236s
 
 import random
+import re
 
 # Make class and initialize it
 class game:
-    def __init__(self, dem_size, num_bomb):
-        self.dem_size = dem_size
+    def __init__(self, dim_size, num_bomb):
+        self.dim_size = dim_size
         self.num_bomb = num_bomb
+    
+        self.board = self.board()
+        self.neighboring_bomb()
+        self.dug = set()
 
 # Make board
     def board(self):
-        board = [[None for i in range(dem_size)] for i in range(dem_size)]
+        # Making board
+        board = [[None for i in range(self.dim_size)] for i in range(self.dim_size)]
+        # Planting bombs
         bomb = 0
-        while bomb < self.num_bom:
-            location = random.randint(0, self.dem_size**2 -1)
-            row = location // self.dem_size
-            column = location % self.dem_size
+        while bomb < self.num_bomb:
+            location = random.randint(0, self.dim_size**2 -1)
+            row, column = location // self.dim_size, location % self.dim_size
+            # column = location % self.dim_size
             if board[row][column] == "x":
                 continue
             board[row][column] == "x"
@@ -29,18 +36,63 @@ class game:
 
 # Set numbers for neighboring bombs
     def neighboring_bomb(self):
-        for row in range(max(0, row-1), min(self.dim_size-1, row+1)):
-            for column in range(max(0, column-1), min(self.dim_size-1, column+1)):
-                if board[row][column] == "x":
+        for row in range(self.dim_size):
+            for column in range(self.dim_size):
+                if self.board[row][column] == "x":
                     continue
-                board[row][column] = self.get_bomb_num(row, columb)
+                self.board[row][column] = self.get_bomb_num(row, column)
 
     def get_bomb_num(self, row, column):
         bomb_count = 0
         for row in range(max(0, row-1), min(self.dim_size-1, row+1)):
             for column in range(max(0, column-1), min(self.dim_size-1, column+1)):
-                if row == crow and column == ccolumn:
+                if row == row and column == column:
                     continue
                 if self.board[row][column] == "x":
                     bomb_count += 1
         return bomb_count
+
+    def dig(self, row, col):
+        self.dug.add((row,col))
+        if self.board[row][col] == "x":
+            return False
+        elif self.board[row][col] > 0:
+            return True
+        for row in range(max(0, row-1), min(self.dim_size-1, row+1)):
+            for column in range(max(0, column-1), min(self.dim_size-1, column+1)):
+                if(row, col) in self.dug:
+                    continue
+                self.dig(row, col)
+        return True
+
+    def __str__(self):
+        visible_board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
+        for row in range(self.dim_size):
+            for col in range(self.dim_size):
+                if (row, col) in self.dug:
+                    visible_board[row][col] = str(self.board[row][col])
+                else:
+                    visible_board[row][col] = " "
+
+def play(dim_size = 10, num_bomb = 10):
+    board = game(dim_size, num_bomb)
+    sage = True
+    while len(board.dug) < board.dim_size ** 2 - num_bomb:
+        print(board)
+        user_input = re.split("\s*,\s*", input("Pick a spot! x,y"))
+        row, col = int(user_input[0]), int(user_input[1])
+        if row <0 or roe >= board.dim_size or col < 0 or col >= board.dim_size:
+            print("Invalid location. Try again.")
+            continue
+        safe = board.dig(row, col)
+        if not sage:
+            break
+    if safe:
+        print("Congratulations! You won!")
+    else:
+        print("Sorry, you lost")
+        board.dug = [(row, col) for row in range(board.dim_size) for c in range(boad.dim_size)]
+        print(board)
+
+if __name__ == "__main__":
+    play()
